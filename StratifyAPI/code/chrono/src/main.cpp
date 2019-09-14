dc3d699cc0888e0db4b545f63e100dd3fb0c5fb0
Tyler Gilbert <tyler.w.gilbert@gmail.com>
2019-09-14 15:15:34 -0600

    a few simple examples

---
+++ StratifyAPI/code/chrono/src/main.cpp
@@ -0,0 +1,22 @@
+
+#include <cstdio>
+//md2code:include
+#include <sapi/chrono.hpp>
+
+int main(int argc, char * argv[]){
+{
+//md2code:main
+wait(Seconds(1)); //wait for one second -- Timer inherits WorkObject so it can use the wait methods statically
+wait(Milliseconds(1000));
+wait(Microseconds(1000000));
+
+ClockTime clock_time;
+wait(clock_time); //wait based on a clock time value
+
+Microseconds microseconds;
+microseconds = Milliseconds(100);
+wait(microseconds); //wait based on a microsecond value
+}
+
+	return 0;
+}
